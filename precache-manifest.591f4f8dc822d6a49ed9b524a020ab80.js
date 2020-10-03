a34da358bac66f36cdcc632730862419c5c6531d
iamgiribabu <girianisetti06@gmail.com>
2020-10-03 16:35:33 +0530

    Updates

---
+++ precache-manifest.591f4f8dc822d6a49ed9b524a020ab80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eeb4b3887c9ea7a608042c0b3e00c95",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "c2ead444992a7670d536",
+    "url": "/covidtracker/static/css/main.1acc6c10.chunk.css"
+  },
+  {
+    "revision": "d9bd38f774ca3730d6e6",
+    "url": "/covidtracker/static/js/2.cbfe2e82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covidtracker/static/js/2.cbfe2e82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2ead444992a7670d536",
+    "url": "/covidtracker/static/js/main.ca68d3f0.chunk.js"
+  },
+  {
+    "revision": "635d99462bdf63fb9823",
+    "url": "/covidtracker/static/js/runtime-main.aeb5b5d1.js"
+  }
+]);
